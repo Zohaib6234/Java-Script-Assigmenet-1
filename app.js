c637c9d4841bf9e089c4dc689bf379d198ff3c05
Zohaib Ali <137928612+Zohaib6234@users.noreply.github.com>
2023-10-13 19:47:20 +0500

    zohaib ali

---
+++ app.js
@@ -0,0 +1,65 @@
+// cheptar 1
+// question 1
+// alert("Zohaib Ali");
+
+
+// question 2
+// alert("Welcome to js \n Happy coding");
+
+// question 3
+// var print = "Hello... I can run Java script through my web browser"
+// alert(print)
+// console.log(print)
+
+// chapter 2
+// question 1
+// var name = "My Name Is Zohaib Ali"
+// alert(name)
+
+// qustion 2
+// var age = "I M 30 Years Old"
+// alert(age)
+
+// question 3
+// var course = "Certefiede Mobile Aplication Development"
+// alert(course)
+
+// qustion 3 
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// alert(pizza)
+
+// question 4
+// var email ="My Email Adress Is Azohaib760@gmail.com"
+// alert(email)
+
+// question 5
+// var learn = "I am trying to learn from the book A smarter way to learn Java script"
+// alert(learn)
+
+// question 6
+// var massege ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(massege)
+
+
+// chapter 3
+
+// question 1
+
+// var age = "I am 30 Years Old"
+// alert(age)
+
+// question 2
+
+// var visit = "You Have Visted This Site 14 Times"
+// alert(visit)
+
+// chapter 4
+// question 1
+
+var name = "My Nmae is Zohaib"
+var age = "i am 30 Years Old"
+var course = "Certefied Mobile Application Development"
+var massege = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(name+" "+age+" "+course+" "+massege)
+
+alert(name+age +course +massege)
